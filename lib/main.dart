2aaa11ab42ab7b96dcbed80a567cf37c7bcfa807
f1reworker <daniil.shalin2406@mail.ru>
2021-08-10 23:20:24 +0300

    Added calendar

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:todo_list/pages/today.dart';
-import 'package:todo_list/pages/calendar.dart';
-import 'package:todo_list/pages/analysis.dart';
-import 'package:todo_list/pages/notes.dart';
+
+import 'models/navigationBar.dart';
 
 const myColor = Color.fromRGBO(20, 20, 20, 100);
 const myColorPrimary = Color.fromRGBO(12, 10, 13, 1);
@@ -15,6 +13,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'My Todo',
       theme: ThemeData(
+          primaryTextTheme:
+              TextTheme(bodyText1: TextStyle(color: Colors.white)),
           scaffoldBackgroundColor: myColor,
           primaryColor: myColorPrimary,
           // ignore: deprecated_member_use
@@ -32,57 +32,5 @@ class HomePage extends StatelessWidget {
       backgroundColor: Theme.of(context).primaryColor,
       body: NavigationBar(),
     ));
-  }
-}
-
-class NavigationBar extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 4,
-      child: Scaffold(
-        body: TabBarView(
-          children: [
-            Today(),
-            Calendar(),
-            Analysis(),
-            Notes(),
-          ],
-        ),
-        bottomNavigationBar: TabBar(
-          tabs: [
-            Tab(
-              icon: Icon(
-                Icons.assignment,
-                size: 27,
-                color: Colors.white,
-              ),
-            ),
-            Tab(
-              icon: Icon(
-                Icons.date_range,
-                size: 27,
-                color: Colors.white,
-              ),
-            ),
-            Tab(
-              icon: Icon(
-                Icons.assessment,
-                size: 27,
-                color: Colors.white,
-              ),
-            ),
-            Tab(
-              icon: Icon(
-                Icons.border_color,
-                size: 27,
-                color: Colors.white,
-              ),
-            ),
-          ],
-        ),
-        backgroundColor: myColorPrimary,
-      ),
-    );
   }
 }
--- lib/pages/calendar.dart
+++ lib/pages/calendar.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:todo_list/main.dart';
+import 'package:syncfusion_flutter_core/theme.dart';
 
-class Calendar extends StatelessWidget {
+class CalendarPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,9 +19,56 @@ class Calendar extends StatelessWidget {
           )
         ],
       ),
-      body: Container(
-        child: Text("Календарь"),
+      body: Calendar(),
+    );
+  }
+}
+
+class Calendar extends StatefulWidget {
+  @override
+  _CalendarState createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SfCalendarTheme(
+        data: SfCalendarThemeData(
+          brightness: Brightness.dark,
+          backgroundColor: myColor,
+        ),
+        child: SafeArea(
+          child: SfCalendar(
+            view: CalendarView.month,
+            firstDayOfWeek: 1,
+            dataSource: MeetingDataSource(getAppointments()),
+            onSelectionChanged: (CalendarSelectionDetails details) {},
+          ),
+        ),
       ),
     );
+  }
+}
+
+List<Appointment> getAppointments() {
+  List<Appointment> meetings = <Appointment>[];
+  final DateTime today = DateTime.now();
+  final DateTime startTime =
+      DateTime(today.year, today.month, today.day, 9, 0, 0);
+  final DateTime endTime = startTime.add(const Duration(hours: 2));
+
+  meetings.add(Appointment(
+      startTime: startTime,
+      endTime: endTime,
+      subject: "Conference",
+      color: Colors.red,
+      recurrenceRule: "FREQ=DAILY;COUNT=10"));
+  return meetings;
+}
+
+class MeetingDataSource extends CalendarDataSource {
+  MeetingDataSource(List<Appointment> source) {
+    appointments = source;
   }
 }
